9aee577b34b2a2bbc966f161d1b63a081b166818
NistonN <nisprogrammer@gmail.com>
2025-08-14 00:54:11 +0300

    DASHBOARD

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { Container } from "./components/Container";
+import { Logo } from "./components/Main/Logo";
+import { navigation } from "./constants/navigation.constants";
 
 function App() {
   return (
     <Container>
       <div className="flex w-full">
         <div>
-          <div className="font-man"></div>
+          <Logo />
+          <div>
+            {navigation.map((link) => (
+              <div className="flex">
+                <div>
+                  <img src={link.logo} alt={`nav-${link.href}`} />
+                </div>
+                <div className="font-manropeExtraBold font-extrabold text-[15px] leading-[24px] tracking-normal align-middle">{link.name}</div>
+              </div>
+            ))}
+          </div>
         </div>
         <div>block2</div>
       </div>
